aa8b8b8cf47148394082ef7611b3bd8e250dc6a6
Fyrid <99447610+Fyrid19@users.noreply.github.com>
2022-03-05 16:57:56 -0500

    made things more clear

--- addThisInPlayState.hx
+++ addThisInPlayState.hx
@@ -1,7 +1,7 @@
 public var curbg:FlxSprite; //add this near public variables
 
 case 'yourbg': //add this near backgrounds
-                var bg:BGSprite = new BGSprite('image', -100, -200, 0.9, 0.9);
+                var bg:BGSprite = new BGSprite('your-image-name', -100, -200, 0.9, 0.9);
                 add(bg);
                 // below code assumes shaders are always enabled which is bad
                 var testshader:Shaders.GlitchEffect = new Shaders.GlitchEffect(2, 5, 0.1);
